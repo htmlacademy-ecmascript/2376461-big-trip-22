86071b1e1d5eb4a78c55fa894bbe14b6d084ae7e
IndieUnityMaker <ubivarik_serega@mail.ru>
2023-12-08 20:58:31 +0300

    презентер

---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,35 @@
+import {render} from '../render.js';
+
+import FiltersView from '../view/filters-view.js';
+import SortView from '../view/sort-view.js';
+import TripListView from '../view/trip-list-view.js';
+import PointView from '../view/point-view.js';
+import InfoView from '../view/info-view.js';
+import FormCeateView from '../view/form-create-view.js';
+import EditView from '../view/form-edit-view.js';
+
+export default class TripPresenter {
+
+  constructor({mainContainer,filterContainer,tripContainer}) {
+    this.mainContainer = mainContainer;
+    this.filterContainer = filterContainer;
+    this.tripContainer = tripContainer;
+  }
+
+  init() {
+    const tripList = new TripListView();
+
+    render(new InfoView(),this.mainContainer,'тзь');
+    render(new FiltersView(), this.filterContainer);
+    render(new SortView(),this.tripContainer);
+    render(tripList,this.tripContainer);
+
+    render(new FormCeateView(),tripList.element,'afterbegin');
+
+    for(let i = 0; i < 3; i++){
+      render(new PointView(),tripList.element);
+    }
+
+    render(new EditView(),tripList.element);
+  }
+}
