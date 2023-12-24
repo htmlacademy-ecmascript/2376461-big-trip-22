eab143463fa90a45be4b343c149e5418fb5270d1
IndieUnityMaker <ubivarik_serega@mail.ru>
2023-12-24 13:51:45 +0300

    модель данных

---
+++ src/Models/points-model.js
@@ -0,0 +1,31 @@
+import { getRandomPoint,getOffersInType,getDestinationsMock } from '../mock/mock-points.js';
+
+const POINT_COUNT = 4;
+
+export default class PointsModel {
+
+  points = Array.from({length: POINT_COUNT}, getRandomPoint);
+
+  getPoints(){
+    return this.points;
+  }
+
+  getDestinations(){
+    return getDestinationsMock();
+  }
+
+  getDestinationById(id){
+    const allDestinations = this.getDestinations();
+    return allDestinations.find((item) => item.id === id);
+  }
+
+  getOffersByType(type){
+    this.offer = getOffersInType().filter((item) =>item.type === type);
+    return this.offer[0].offers;
+  }
+
+  getDestinationNameList(){
+    const destinations = this.getDestinations();
+    return destinations.map((item) => item.name);
+  }
+}
