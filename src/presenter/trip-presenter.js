839a51967023990b1bc489013920201fd081be96
IndieUnityMaker <ubivarik_serega@mail.ru>
2023-12-27 18:51:24 +0300

    import framework render

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,4 +1,4 @@
-import {render,RenderPosition} from '../render.js';
+import {render,RenderPosition} from '../framework/render.js';
 import { BLANK_CREATE_FORM } from '../constants.js';
 
 import FiltersView from '../view/filters-view.js';
