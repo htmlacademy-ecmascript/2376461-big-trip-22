3ae7d2323219afd94e9ea03f99a98831bb86961a
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-01-03 21:12:18 +0300

    #

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-function getRandomArrayElement(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
-const typeNameNormalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);// taxi - Taxi
-
-export { getRandomArrayElement,typeNameNormalize };
