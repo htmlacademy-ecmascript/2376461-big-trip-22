007d557627490ff1740b4e7351fe1ba848f59ea4
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-01-24 19:11:27 +0300

    универсальные функции

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,6 +7,22 @@ function getRandomArrayElement(items) {
  * @param {string} str Строка
  */
 const typeNameNormalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+/**
+ * Получить массив значений ключа
+ * @param {string} key Ключ по которому будет собран массив
+ * @param {Array} arr Массив обьектов
+ */
+const getAllKeyValue = (key,arr) => arr.map((item) => item[key]);
+/**
+ * Получить item из массива указав его id
+ * @param {Number} id id по которому будет найден item
+ * @param {Array} arr Массив обьектов
+ */
+const getItemById = (id,arr) => arr.find((item) => item.id === id);
 
+//обновление элементов в массиве объектов
+function updateItem(items, update) {
+  return items.map((item) => item.id === update.id ? update : item);
+}
 
-export { getRandomArrayElement, typeNameNormalize };
+export { getRandomArrayElement, typeNameNormalize, getAllKeyValue, getItemById, updateItem };
