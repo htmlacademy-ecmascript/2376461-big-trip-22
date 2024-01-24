a2152fe0535d6507cb8f89a8f291a5e2392959e9
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-01-24 19:10:31 +0300

    #

--- src/Models/points-model.js
+++ src/Models/points-model.js
@@ -14,18 +14,8 @@ export default class PointsModel {
     return getDestinationsMock();
   }
 
-  getDestinationById(id){
-    const allDestinations = this.getDestinations();
-    return allDestinations.find((item) => item.id === id);
-  }
-
   getOffersByType(type){
     this.offer = getOffersInType().filter((item) =>item.type === type);
     return this.offer[0].offers;
-  }
-
-  getDestinationNameList(){
-    const destinations = this.getDestinations();
-    return destinations.map((item) => item.name);
   }
 }
