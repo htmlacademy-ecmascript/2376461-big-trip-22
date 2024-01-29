fb027537c03918a6d28bb7b0bc958211deb7b765
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-01-29 21:05:19 +0300

    отредактировал пустышку точки

--- src/constants.js
+++ src/constants.js
@@ -1,47 +1,16 @@
 
-const BLANK_CREATE_FORM = {
-  point: {
-    type: 'bus',
-    destination: 'dest-id-1',
-    timeDateStart: new Date().toString(),//день/месяц/год часы:минуты  flatpicker: d/m/Y h:i
-    timeDateEnd: new Date().toString(),
-    price: 100,
-    isFavorite: false,
-    offers: [0],
-  },
-  offers: {
-    type: 'bus',
-    offers: [
-      {
-        id: 0,
-        title: 'bus upgrade 1',
-        price: '31'
-      },
-      {
-        id: 1,
-        title: 'bus upgrade 2',
-        price: '34'
-      },
-      {
-        id: 2,
-        title: 'bus upgrade 3',
-        price: '33'
-      },
-    ],
-  },
-  destination: {
-    id: 'dest-id-1',
-    description: 'Monika destination',
-    name: 'Monika',
-    pictures: []
-  },
-  destinationsName: [
-    'Monika',
-    'Mongolia',
-    'USA',
-    'Thailand',
-    'Home']
+const BLANK_CREATE_POINT = {
+  id: '0',
+  type: 'taxi',
+  destination: '',
+  timeDateStart: new Date().toString(),//день/месяц/год часы:минуты  flatpicker: d/m/Y h:i
+  timeDateEnd: new Date().toString(),
+  price: 0,
+  isFavorite: false,
+  offers: [],
 };
+
+const POINTS_TYPE = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
 
 const FiltersType = {
   everything: 'everything',
@@ -70,4 +39,4 @@ const SortType = {
   OFFERS: 'offers'
 };
 
-export { BLANK_CREATE_FORM, FiltersType, CLEAR_BOARD_TEXT , Mode, SortType };
+export { BLANK_CREATE_POINT, POINTS_TYPE, FiltersType, CLEAR_BOARD_TEXT , Mode, SortType };
