c5c56c1040fb99783094b2e23a114e44c075edc5
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-02-03 00:35:53 +0300

    кординально изменил модель, стало очень сложно и умно

--- src/Models/points-model.js
+++ src/Models/points-model.js
@@ -1,29 +1,64 @@
-import { getRandomPoint,getOffersInType,getDestinationsMock } from '../mock/mock-points.js';
+import { getOffersInType,getDestinationsMock, getPointsMock } from '../mock/mock-points.js';
+import { updateItem } from '../utils/common.js';
+import Observable from '../framework/observable.js';
 
-const POINT_COUNT = 5;
-
-
-export default class PointsModel {
-  #points = [];
+export default class PointsModel extends Observable {
+  #points = getPointsMock();
   #offers = getOffersInType();
+  #destinations = getDestinationsMock();
 
-  getPoints(){
-    //забиваю массив точками, но только уникальными
-    while(this.#points.length < POINT_COUNT){
-      const point = getRandomPoint();
-      if(!this.#points.includes(point)){
-        this.#points.push(point);
-      }
-    }
-
-    return this.#points;
+  get wayPoints(){
+    return this.#points;//получить точки
   }
 
-  getDestinations(){
-    return getDestinationsMock();
+  set wayPoints(points) {
+    this.#points = [...points];
   }
 
-  getOffers(){
-    return this.#offers;
+  get destinations(){
+    return this.#destinations;//получить назначения
   }
+
+  set destinations(destinations){
+    this.#destinations = [...destinations];
+  }
+
+  get offers(){
+    return this.#offers;//получить предложения
+  }
+
+  set offers(offers){
+    this.#offers = [...offers];
+  }
+
+  //получить данные о точке маршрута по её идентификатору
+  getPointDataById(id) {
+    const point = this.#points.find((item) => item.id === id);
+    const destination = this.#destinations.find((item) => item.id === point.destination);
+    const offers = this.#offers.find((item) => item.type === point.type.toLocaleLowerCase());
+    return {
+      point: point ?? {},
+      destination: destination ?? {},
+      offers: offers.offers ?? {}
+    };
+  }
+
+  //обновить точку маршрута
+  updatePoint(updateType, updatedPoint) {
+    this.#points = updateItem(this.#points, updatedPoint);
+    this._notify(updateType, updatedPoint.id);
+  }
+
+  //добавить точку маршрута
+  addPoint(updateType, newPoint) {
+    this.#points.push(newPoint);
+    this._notify(updateType);
+  }
+
+  //удалить точку маршрута
+  deletePoint(updateType, point) {
+    this.#points = this.#points.filter((item) => item.id !== point.id);
+    this._notify(updateType);
+  }
+
 }
