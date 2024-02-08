a1c0026e69ddd66f8d850b6b5e563ed9bdcf0654
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-02-08 19:31:05 +0300

    адаптер и работа с сервером

---
+++ src/trip-api-service.js
@@ -0,0 +1,61 @@
+import ApiService from './framework/api-service';
+import { ApiMethod } from './constants';
+
+
+export default class TripApiService extends ApiService {
+
+  get points () {
+    return this._load({
+      url: 'points'
+    }).then(ApiService.parseResponse);
+  }
+
+  get destinations () {
+    return this._load({
+      url: 'destinations'
+    }).then(ApiService.parseResponse);
+  }
+
+  get offers () {
+    return this._load({
+      url: 'offers'
+    }).then(ApiService.parseResponse);
+  }
+
+  async addEvent(event) {
+    const response = await this._load({
+      url: 'points',
+      method: ApiMethod.POST,
+      body: JSON.stringify(this.#adaptToServer(event)),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    return await ApiService.parseResponse(response);
+  }
+
+  async deleteEvent(event) {
+    return await this._load({
+      url: `points/${event.id}`,
+      method: ApiMethod.DELETE,
+    });
+  }
+
+  #adaptToServer(event) {
+    const adaptedEvent = {
+      ...event,
+      'date_from': event.timeDateStart.toISOString(),
+      'date_to': event.timeDateEnd.toISOString(),
+      'base_price': parseInt(event['price'], 10),
+      'is_favorite': event['isFavorite'],
+    };
+
+    // remove unnecessary keys
+    delete adaptedEvent['timeDateStart'];
+    delete adaptedEvent['timeDateEnd'];
+    delete adaptedEvent['price'];
+    delete adaptedEvent['isFavorite'];
+
+    return adaptedEvent;
+  }
+
+}
